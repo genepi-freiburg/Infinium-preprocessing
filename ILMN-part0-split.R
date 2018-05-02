e118fe759da498880dcf1fffc58e65fe6bcfc7e3
Franziska Grundner-Culemann <grundner@imbi.uni-freiburg.de>
2018-05-02 10:29:01 +0200

    added 450k annotationfile and updated comments of where to find the files

--- ILMN-part0-split.R
+++ ILMN-part0-split.R
@@ -15,11 +15,16 @@ require(DescTools)
 
 if (arraytype=="IlluminaHumanMethylationEPIC") {
 	annotationfile="../annotationfileB4_2017-09-15.csv"
+	# this is identical to the file 
+	# MethylationEPIC_v-1-0_B4.csv
+	# which one can download from 
+	# https://support.illumina.com/array/array_kits/infinium-methylationepic-beadchip-kit/downloads.html 
 } else if  (arraytype=="IlluminaHumanMethylation450k") {
-	# annotationfile="../annotationfileB4_2017-09-15.csv"
-	# CHANGE: ILMN array annotation file
-	# stop("Please provide an annotation file for : ",arraytype,". Exiting...\n")
-	stop(paste(arraytype, "is still in setup phase. Exiting...\n"))
+	annotationfile="../annotationfile450k.csv"
+	# this is identical to the file 
+	# HumanMethylation450_15017482_v1-2.csv
+	# which can be downloaded from 
+	# https://support.illumina.com/array/array_kits/infinium_humanmethylation450_beadchip_kit.html	
 } else {
     stop("Unsupported array type: ",arraytype,". Exiting...\n")
 }
