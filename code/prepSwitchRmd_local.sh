996ea67bd4ba421218a687f28dd562d23d4a236b
Franziska Grundner-Culemann <grundner@imbi.uni-freiburg.de>
2018-06-18 14:02:22 +0200

    Due to pandoc error and to avoid overwriting when program runs parallel, now a new directory is created at the folder from which the pipeline is called and all is processed in that folder. The temporary folder has a time stamp and is deleted afterwards. This version works fine.

---
+++ code/prepSwitchRmd_local.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+tmpfolder=temporary_EPIC_pipeline_code_`date "+%Y%m%d-%H%M%S"`
+mkdir $tmpfolder
+cp /data/programs/pipelines/CPACOR-EPIC_pipeline/code/*  $tmpfolder/
+
+cd $tmpfolder/
+
+time R-3.4.1 --vanilla << "EOF"
+.libPaths()
+getwd()
+
+ source("../parameterfile.R")
+ library(rmarkdown)
+ parameterpath = paste0("../parameterfile.R")
+ callpath=getwd()
+
+  if(QuantileNormalize == FALSE){
+  	 rmarkdown::render(paste0("prep12.Rmd"),  output_file = paste0(format(Sys.time(),format = "%Y-%m-%d-%Hh%Mm"),"-",projectname,"-QC",".pdf"), clean = FALSE, output_dir = callpath , params = list(parameterfile = parameterpath) )
+   } else {
+	if(InterQuartileRangeCalculation){
+  	 rmarkdown::render(paste0("prep1234.Rmd"), output_file = paste0(format(Sys.time(),format = "%Y-%m-%d-%Hh%Mm"),"-",projectname,".pdf"),clean = FALSE, output_dir = callpath , params = list(parameterfile = parameterpath) )
+	} else {
+  	 rmarkdown::render(paste0("prep123.Rmd"), output_file = paste0(format(Sys.time(),format = "%Y-%m-%d-%Hh%Mm"),"-",projectname,".pdf"), clean = FALSE, output_dir = callpath, params = list(parameterfile = parameterpath) )
+	}
+   } 
+
+EOF
+
+cp *.pdf ..
+cp -r *_files ..
+cd ..
+rm -r $tmpfolder
