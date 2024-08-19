4ce3814cb6600b9f1cf66f908d797a40fe751ede
zulemarodriguez <zuleyvito@gmail.com>
2024-08-19 10:58:33 +0200

    Added baseline parameterfile.R and submissionscript

---
+++ parameterfile.R
@@ -0,0 +1,59 @@
+# for using this pipeline, please install all packages listed in the code file "prepPart0.R"
+
+arraytype<-"IlluminaHumanMethylationEPIC"
+# SELECT  between "IlluminaHumanMethylation450k" and "IlluminaHumanMethylationEPIC"
+# (see minfi array names)
+
+##################################
+# define files and directories ###
+
+# name the documentation (the data file names remain unchanged)
+projectname="GCKD_batch1and2and3_test"
+# CHANGE: any string; date and time are appended in the code to prevent overwriting
+
+idatdir="/data/studies/00_GCKD/00_data/02_methylation/00_raw_data/EPIC_batch1and2/ScanData;/data/studies/00_GCKD/00_data/02_methylation/00_raw_data/EPIC_batch3/M01157_Köttgen/idat"
+# CHANGE: directory containing all idat files, absolute path, no / at the end, separate paths with ;
+
+outputdir="/data/studies/00_GCKD/00_data/02_methylation/01_QC/03_DNAm_preprocessing_batches123/tests_ZRH"
+# CHANGE: output directory for RData files, absolute path, no / at the end
+
+
+#########################################
+# samplesfile needed for QC (steps 2) ###
+
+samplesfile="/data/studies/00_GCKD/00_data/02_methylation/01_QC/03_DNAm_preprocessing_batches123/tests_ZRH/samplesheet_GCKD_batch1and2_testfailplus5.txt"
+# CHANGE: sample table; must be absolute path and not relative path
+
+
+samplesfilefinal=samplesfile
+# CHANGE: revised sample table for steps 3+; samplesfilefinal=samplesfile is possible 
+
+
+# Format: tab-delimited text, columns: Gender (0-males, 1-females), Sample_ID (array/idat name)
+# example samplesfile:
+#Sample_Name    Sample_Well    Sample_Plate    Sample_Group    Pool_ID    Sentrix_ID    Sentrix_Position    Sample_ID    Gender
+#4342    A01    1    RS3_pilot_450K        5723654049    R01C01    5723654049_R01C01    1
+#9797    B01    1    RS3_pilot_450K        5723654049    R02C01    5723654049_R02C01    0
+
+############################
+# Only needed if there are more batch variables then Sample_Plate and Sample_Well that should be tested for batch effects. 
+# provide full path 
+BatchVariablesFile=NULL
+
+############################
+# threshold parameters ###
+
+detPthreshold=1E-16
+callrate.thres=0.95
+
+
+##############
+# switches ###
+# CHANGE only between TRUE and FALSE
+
+filterOutlierCtrlQC=TRUE
+QuantileNormalize=TRUE
+InterQuartileRangeCalculation=TRUE
+estimateWBCs=FALSE
+extractSNPs=TRUE
+additionalBatchVariables=FALSE
---
+++ submissionScript-preprocessing.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#SBATCH -n1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100G
+
+# SBATCH --mail-user=CHANGE@imbi.uni-freiburg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --partition=slurm 
+#SBATCH --output ../slurm-log-%A.txt
+
+
+sh /data/programs/pipelines/CPACOR-EPIC_pipeline/code/prepSwitchRmd_local.sh 
+
+# This file can be queued with $ sbatch /data/programs/pipelines/CPACOR-EPIC_pipeline/submissionScript-preprocessing.sh
+
+# It is necessary to provide a file parameterfile.R in the very same directory from which the job is batched. 
+# The parameterfile may not be changed until the job is started, which may be later than its submission.
+
+# Parameters can be overwritten on the commandline. 
+# To increase memory to X megabyte per node, add --mem=X
+# To increase memory to X megabyte per cpu instead, add --mem-per-cpu=X
+# To change to partition X, add --partition=X
+# To get mail when the job is finished, add: --mail-user=USER@imbi.uni-freiburg.de
+# Mail type options are BEGIN, END, FAIL, REQUEUE, ALL
+# It has no effect if one increases the number of cpus.
+
+
+# NOTE 80 samples used 20 GB.
+# NOTE 600 samples used > 40 GB.
+# NOTE Different runs at the same time overwrite each other. 
